564dd8e2cd4589d404b1a06121d3cf61f04b61d5
phharoah <eng.ahmed7gazy@exgmail.com>
2025-09-15 11:47:25 +0300

    finish mobile layout

---
+++ lib/utils/my_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_ui/views/screens/home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomeView());
+  }
+}
